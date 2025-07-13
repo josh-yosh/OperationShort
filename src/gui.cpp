b88df4d71ecd20f409cc2ef2635485e6c8543287
JayYosh <133554802+JayYosh@users.noreply.github.com>
2025-07-12 20:12:01 -0700

    created positions for windows

--- src/gui.cpp
+++ src/gui.cpp
@@ -1,19 +1,24 @@
 #define GLFW_INCLUDE_NONE
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "UsingImGui.h"
 
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <iostream>
 #include <imgui.h>
 
+using namespace std;
+
 
 int main() {
+    
     if (!glfwInit()) {
         std::cerr << "Failed to initialize GLFW\n";
         return -1;
     }
 
+    const char *glsl_version = "#version 410";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
@@ -25,7 +30,7 @@ int main() {
     }
 
     glfwMakeContextCurrent(window);
-    // ImGui::StyleColorsDark();
+    glfwSwapInterval(1); 
 
     // Initialize GLAD **after** context creation
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -33,14 +38,26 @@ int main() {
         return -1;
     }
 
+    int screen_width, screen_height;
+	glfwGetFramebufferSize(window, &screen_width, &screen_height);
+	glViewport(0, 0, screen_width, screen_height);
+
+    UseImGui myimgui;
+    myimgui.Init(window, glsl_version);
+
+
     while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        myimgui.NewFrame();
+        myimgui.Update();
+        myimgui.Render();
 
         glfwSwapBuffers(window);
-        glfwPollEvents();
     }
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
+    myimgui.Shutdown();
     return 0;
 }
